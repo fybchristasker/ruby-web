df195ca28f1dc131fb6867671cb81e5a45d74136
fyb <fyb@paiyou.co>
2019-11-16 13:39:07 +0800

    fix

--- pages/_app.js
+++ pages/_app.js
@@ -2,14 +2,15 @@ import React from 'react'
 import App from 'next/app'
 import Head from '../components/head'
 import Nav from '../components/nav'
-import 'antd/dist/antd.css'
 import "../styles/main.scss"
+import { Button } from 'antd'
 
 class MyApp extends App {
   render() {
     const { Component, pageProps } = this.props
     return <div>
       <Head />
+      <Button>sfkjskjsfksfkjkjkkjk</Button>
       <Component {...pageProps} />
     </div>
   }
