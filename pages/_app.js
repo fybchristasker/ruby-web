f7a1a70a412908807e59b3aaa0bf9fa6444c769d
fyb <fyb@paiyou.co>
2019-11-29 11:11:54 +0800

    add material ui

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,17 @@
 import React from 'react'
 import App from 'next/app'
 import Head from '../components/head'
-import "../styles/main.scss"
+import '../styles/main.scss'
 
 class MyApp extends App {
   render() {
     const { Component, pageProps } = this.props
-    return <div>
-      <Head />
-      <Component {...pageProps} />
-    </div>
+    return (
+      <div>
+        <Head />
+        <Component {...pageProps} />
+      </div>
+    )
   }
 }
 
--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,15 @@
 import React from 'react'
+import Button from '@material-ui/core/Button'
 
 const IndexPage = () => {
   return (
     <div style={{ width: '100%', height: '100vh', zIndex: '-1' }} className="gradient-background-anim">
       <div className="index-title">龅 牙 梨 花 珍</div>
+      <div className="TA-C MT-20">
+        <Button variant="contained" color="primary">
+          WELCOME TO RUBY
+        </Button>
+      </div>
     </div>
   )
 }
