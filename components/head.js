a63b900d39d03f0a0b7452ea2bc35384fd29c7e0
fyb <fyb@paiyou.co>
2019-12-04 20:17:13 +0800

    fix

--- components/head.js
+++ components/head.js
@@ -5,12 +5,12 @@ const Head = () => (
   <NextHead>
     <meta charSet="UTF-8" />
     <title>龅牙梨花珍</title>
+    <meta httpEquiv="content-type" content="text/html; charset=utf-8" />
+    <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+    <meta name="keywords" content="龅牙梨花珍" />
     <meta name="description" content="龅牙梨花珍" />
     <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=1, user-scalable=no" />
-    <meta property="og:title" content="龅牙梨花珍" />
-    <meta name="twitter:title" content="龅牙梨花珍" />
     <link rel="icon" href="/static/favicon.ico" />
-    <meta name="keywords" content="龅牙梨花珍" />
   </NextHead>
 )
 
