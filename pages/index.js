e9bcf017e6d21525694f2359e817181a337e1852
fyb <fyb@paiyou.co>
2020-01-02 14:17:29 +0800

    upload

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,30 @@
-import React, { useState } from 'react'
+import React from 'react'
 import Button from '@material-ui/core/Button'
-import Modal from '@material-ui/core/Modal'
 
 const IndexPage = () => {
-  const [open, setOpen] = useState(false)
+  const upload = file => {
+    const formData = new FormData()
+    formData.append('smfile', file)
+
+    fetch('https://sm.ms/api/v2/upload', {
+      headers: { Authorization: 'dFj8URDsnK33D4cyA334eQH16MRiprR9' },
+      method: 'POST',
+      body: formData
+    })
+      .then(response => response.json())
+      .then(body => {
+        console.info('body', body)
+      })
+      .catch(error => console.error(error))
+  }
   return (
     <div style={{ width: '100%', height: '100vh', zIndex: '-1' }} className="gradient-background-anim">
       <div className="index-title">龅 牙 梨 花 珍</div>
       <div style={{ marginTop: '10vh', textAlign: 'center' }}>
-        <Button variant="contained" color="primary" onClick={() => setOpen(true)}>
+        <Button variant="contained" color="primary" onClick={() => upload()}>
           WELCOME TO RUBY
         </Button>
       </div>
-      <Modal open={open} style={{ marginTop: '20vh' }} onClose={() => setOpen(false)}>
-        <div>龅牙梨花珍</div>
-      </Modal>
     </div>
   )
 }
