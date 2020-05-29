c0d93b608d5cea1388643a35d3e4a52bb2a58108
fyb <fyb@paiyou.co>
2020-05-29 17:03:08 +0800

    rm antd

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,12 @@
 import React from 'react'
-import { Button } from 'antd'
+import Button from '@material-ui/core/Button'
 
 const IndexPage = () => {
   return (
     <div style={{ width: '100%', height: '100vh', zIndex: '-1' }} className="gradient-background-anim">
       <div className="index-title">龅 牙 梨 花 珍</div>
       <div style={{ marginTop: '10vh', textAlign: 'center' }}>
-        <Button type="primary">WELCOME TO RUBY</Button>
+        <Button color="primary">WELCOME TO RUBY</Button>
       </div>
     </div>
   )
