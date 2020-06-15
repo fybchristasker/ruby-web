0ecf3992e2c458f3a5b4d6a425488a527f4eb202
fyb <fyb@paiyou.co>
2020-06-15 20:21:49 +0800

    fix scss error

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import App from 'next/app'
 import Head from '../components/head'
-import '../styles/main.scss'
+import 'styles/main.scss'
 
 class MyApp extends App {
   render() {
--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,5 @@
 import React from 'react'
-import Button from '@material-ui/core/Button'
-import 'styles/main.scss'
+import { Button } from '@material-ui/core'
 
 const IndexPage = () => {
   return (
