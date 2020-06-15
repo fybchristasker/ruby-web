d25fb894444f05cf20369dc2ffb2b8820696ad71
fyb <fyb@paiyou.co>
2020-06-15 20:08:23 +0800

    modify text

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ const IndexPage = () => {
     <div style={{ width: '100%', height: '100vh', zIndex: '-1' }} className="gradient-background-anim">
       <div className="index-title">龅 牙 梨 花 珍</div>
       <div style={{ marginTop: '10vh', textAlign: 'center' }}>
-        <Button color="primary">WELCOME TO RUBY</Button>
+        <Button color="primary">WELCOME</Button>
       </div>
     </div>
   )
