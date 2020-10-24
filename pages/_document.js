b9bda8cce45699dc00f50dceae67013ff416f04d
fyb <fyb@paiyou.co>
2020-10-24 15:17:51 +0800

    import material css

--- pages/_document.js
+++ pages/_document.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Document, { Html, Head, Main, NextScript } from 'next/document'
+import { ServerStyleSheets } from '@material-ui/core/styles'
 
 export default class MyDocument extends Document {
   render() {
@@ -12,5 +13,19 @@ export default class MyDocument extends Document {
         </body>
       </Html>
     )
+  }
+}
+
+MyDocument.getInitialProps = async ctx => {
+  const sheets = new ServerStyleSheets()
+  const originalRenderPage = ctx.renderPage
+  ctx.renderPage = () =>
+    originalRenderPage({
+      enhanceApp: App => props => sheets.collect(<App {...props} />)
+    })
+  const initialProps = await Document.getInitialProps(ctx)
+  return {
+    ...initialProps,
+    styles: [...React.Children.toArray(initialProps.styles), sheets.getStyleElement()]
   }
 }
