664faefd7685dc04325664a091d5fa83fac8ab89
fyb <fyb@paiyou.co>
2020-11-28 10:58:09 +0800

    fix button

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ const IndexPage = () => {
     <div style={{ width: '100%', height: '100vh', zIndex: '-1' }} className="gradient-background-anim">
       <div className="index-title">龅 牙 梨 花 珍</div>
       <div style={{ marginTop: '10vh', textAlign: 'center' }}>
-        <Button color="primary" variant="contained">
+        <Button color="primary" variant="outlined">
           WELCOME
         </Button>
       </div>
